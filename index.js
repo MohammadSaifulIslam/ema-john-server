2b0f3f90278769e42d1037738d90f6b2d466e1c7
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2023-05-15 08:48:37 -0700

    filtering limit data by page and limit from mongodb and send to client side function added

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middlewire 
+// middlewire
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kgqetuh.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,16 +18,38 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productsCollection = client.db("emaJohnDB").collection("products");
+
+    app.get("/products", async (req, res) => {
+      const page = parseInt(req.query.page) || 1;
+      const limit = parseInt(req.query.limit) || 9;
+      const skip = (page - 1) * limit;
+
+
+      const result = await productsCollection.find().skip(skip).limit(limit).toArray();
+      res.send(result);
+    });
+
+    app.get('/totalProducts', async(req, res)=>{
+      const result = await productsCollection.estimatedDocumentCount();
+      res.send({totalProducts : result})
+    });
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -37,12 +57,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/',(req, res)=>{
-    res.send("Welcome to Ema-Jhon server")
+app.get("/", (req, res) => {
+  res.send("Welcome to Ema-Jhon server");
 });
 
-app.listen(port, ()=>{
-    console.log(`ema-john server is running on port: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`ema-john server is running on port: ${port}`);
+});
